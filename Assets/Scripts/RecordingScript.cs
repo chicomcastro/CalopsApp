f96855d1e85dde5cf4b1e9f938b59bc7cf67706a
chicomcastro <Franciscommcastro@gmail.com>
2017-05-28 15:35:41 -0300

    Hackaton final commit!
    End!

--- Assets/Scripts/RecordingScript.cs
+++ Assets/Scripts/RecordingScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class RecordingScript : MonoBehaviour {
@@ -10,16 +11,23 @@ public class RecordingScript : MonoBehaviour {
 
 	void Awake () {
 		aud = GetComponent<AudioSource>();
+		if (System.IO.Directory.Exists (System.IO.Directory.GetCurrentDirectory () + "/ConsoleApplication/Ata0/")) {
+			System.IO.Directory.Delete (System.IO.Directory.GetCurrentDirectory () + "/ConsoleApplication/Ata0/");
+			System.IO.Directory.CreateDirectory (System.IO.Directory.GetCurrentDirectory () + "/ConsoleApplication/Ata0/");
+		}
+
 	}
 
 	public void StartRecording () {
 		if (!Microphone.IsRecording (currenteMic)) {
+			GameObject.FindGameObjectWithTag ("Canvas").GetComponent<Animator> ().SetBool ("active", true);
 			aud.clip = Microphone.Start (currenteMic, false, 30, 44100);
 		}
 	}
 
 	public void StopRecording () {
 		if ( Microphone.IsRecording (currenteMic) ) {
+			GameObject.FindGameObjectWithTag ("Canvas").GetComponent<Animator> ().SetBool ("active", false);
 			Microphone.End (currenteMic);
 			GenerateDocument ();
 		}
@@ -28,12 +36,18 @@ public class RecordingScript : MonoBehaviour {
 		GetComponent<GenerateAudio> ().CallSaveFunction("audio", aud.clip);
 	}
 
+	// If you want to listen to last recorded audio
 	public void ListenTo () {
 		if (!aud.isPlaying) {
 			aud.Play ();
 		} else {
 			aud.Stop ();
 		}
+	}
+
+	// Read generate .txt files
+	public void Read () {
+		System.Diagnostics.Process.Start (System.IO.Directory.GetCurrentDirectory () + "/ConsoleApplication/Ata0/");
 	}
 
 	public void ExitApplication () {
